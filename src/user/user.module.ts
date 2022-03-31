571bd25519065176c87bf57006d48e63312017a4
MrSandyC <sandercoret@gmail.com>
2022-03-31 07:19:44 +0200

    feat: added start of authorization and registration

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'User-service',
+        transport: Transport.TCP,
+        options: {
+          port: 5002,
+        },
+      },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { RegisterUserRequest } from './dto/register-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @Inject('User-service') private readonly userClient: ClientProxy,
+    @Inject('Queet-service') private readonly queetClient: ClientProxy,
+  ) {}
+  fetchAllUsers() {
+    return this.userClient.send('user:find-all', {});
+  }
+
+  createUser(registerUserRequest: RegisterUserRequest) {
+    this.queetClient.emit('user:register', registerUserRequest);
+    return this.userClient.emit('user:register', registerUserRequest);
+  }
+
+  fetchUserById(id: number) {
+    return this.userClient.send('user:find-by-id', id);
+  }
+
+  checkIfUserExists(auth0id: string) {
+    return this.userClient.send('user:check-if-exists', auth0id);
+  }
+
+  removeUser(id: number) {
+    return this.userClient.emit('user:remove', id);
+  }
+
+  updateUser(updateUserDto: UpdateUserDto) {
+    return this.userClient.emit('user:update', updateUserDto);
+  }
+}
