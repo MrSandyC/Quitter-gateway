4df3f6a860b0f8d2a74a28fa3e220ac6d6ee5aa5
MrSandyC <sandercoret@gmail.com>
2022-04-01 14:40:16 +0200

    feat: added follow controller etc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Quitter Swagger')
+    .setDescription('API description for the swagger project')
+    .setVersion('1.0')
+    .addTag('quitter')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
